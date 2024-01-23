0e27722938a3aab6f7bb7d8fd65faa9306bcac88
Alscof <132270445+Alscof@users.noreply.github.com>
2024-01-24 01:50:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ from langchain_community.tools import DuckDuckGoSearchRun
 import streamlit as st
 
 
-st.title('bot')
-# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
-
-os.environ["DISCORD_TOKEN"] = st.secrets["DISCORD_TOKEN"]
-TOKEN: Final[str] = os.environ["DISCORD_TOKEN"]
-
-
 # STEP 1: BOT SETUP
 intents: Intents = Intents.default()
 intents.message_content = True  # NOQA
